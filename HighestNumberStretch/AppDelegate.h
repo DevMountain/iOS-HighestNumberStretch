e9dd9396c8bf94436da51ffb0801062d705cefbc
Michael Sacks <michaelsacks@gmail.com>
2015-12-07 15:45:26 -0700

    Initial Commit

---
+++ HighestNumberStretch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HighestNumberStretch
+//
+//  Created by Michael Sacks on 12/7/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
