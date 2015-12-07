cf360a264a7cc2a8b08d3e06070643cc03e47751
Michael Sacks <michaelsacks@gmail.com>
2015-12-07 15:46:33 -0700

    Solution

--- HighestNumberStretch/ViewController.m
+++ HighestNumberStretch/ViewController.m
@@ -6,9 +6,17 @@
 //  Copyright © 2015 DevMountain. All rights reserved.
 //
 
+// 1. Make a new Objective-C Project
+//
+// 2. Write a method that prints the highest number in an array (that is stored as a property).
+//
+// 3. Change your method to take an array as a parameter and return the highest number. Once you do this, you can comment out your property and just make an array to use.
+
 #import "ViewController.h"
 
 @interface ViewController ()
+
+//@property (strong, nonatomic) NSArray *arr;
 
 @end
 
@@ -16,12 +24,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //    self.arr = @[@4, @3, @53, @212, @0, @8];
+    //    [self highest];
+    
+    NSArray *array2 = @[@4, @3, @53, @212, @0, @8];
+    
+    NSLog(@"Highest number: %@!", [self returnHighest:array2]);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//- (void)highest {
+//
+//    NSNumber *highest = self.arr[0];
+//
+//    for (int i = 0; i < self.arr.count; i++) {
+//        if (self.arr[i] > highest) {
+//            highest = self.arr[i];
+//        }
+//    }
+//
+//    NSLog(@"highest value: %@!", highest);
+//
+//}
+
+- (NSNumber *)returnHighest: (NSArray *) ourArray {
+    
+    NSNumber *highest2 = ourArray[0];
+    
+    for (int i = 0; i < ourArray.count; i++) {
+        
+        if (ourArray[i] > highest2) {
+            highest2 = ourArray[i];
+        }
+    }
+    
+    return highest2;
 }
 
 @end
